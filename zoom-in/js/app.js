be84e0a670909f1a426df2bb7491c333353159d3
alireza talebizadeh <alireza.talebizadeh78@gmail.com>
2023-09-15 15:47:48 +0330

    create animation zoom in

---
+++ zoom-in/js/app.js
@@ -0,0 +1,21 @@
+let imgContainer = document.querySelector('.container')
+
+let img = document.querySelector('img')
+
+
+
+imgContainer.addEventListener('mousemove', event => {
+    // console.log(event.clientX, event.clientY, event.target.offsetLeft,
+    //     event.target.offsetTop);
+
+    const xPos = event.clientX - event.target.offsetLeft
+    const yPos = event.clientY - event.target.offsetTop
+
+    img.style.transformOrigin = `${xPos}px ${yPos}px`
+    img.style.transform = 'scale(2)'
+})
+
+imgContainer.addEventListener('mouseleave', event => {
+    img.style.transformOrigin = `center`
+    img.style.transform = 'scale(1)'
+})
